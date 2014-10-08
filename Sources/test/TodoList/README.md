bc8c67e24c0713371fdf2e2867fbbc290a9668c7
Valerian <polskataga@gmail.com>
2014-10-08 15:27:37 +0200

    creation test todolist

---
+++ Sources/test/TodoList/README.md
@@ -0,0 +1,7 @@
+application Todo-list utilisant node.js
+
+Source : http://fr.openclassrooms.com/informatique/cours/des-applications-ultra-rapides-avec-node-js/tp-la-todo-list
+
+Utiliser la commande npm install pour les modules express et ejs
+
+Lancer node app.js
---
+++ Sources/test/TodoList/app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var app = express();
+
+/* On utilise les cookies, les sessions et les formulaires */
+app.use(express.cookieParser())
+.use(express.session({secret: 'todotopsecret'}))
+.use(express.bodyParser())
+
+/* S'il n'y a pas de todolist dans la session,
+on en crée une vide sous forme d'array avant la suite */
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
+    }
+    next();
+})
+
+/* On affiche la todolist et le formulaire */
+.get('/todo', function(req, res) { 
+    res.render('todo.ejs', {todolist: req.session.todolist});
+})
+
+/* On ajoute un élément à la todolist */
+.post('/todo/ajouter/', function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
+    }
+    res.redirect('/todo');
+})
+
+/* Supprime un élément de la todolist */
+.get('/todo/supprimer/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+/* On redirige vers la todolist si la page demandée n'est pas trouvée */
+.use(function(req, res, next){
+    res.redirect('/todo');
+})
+
+.listen(8080);
