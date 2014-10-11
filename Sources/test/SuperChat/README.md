d333aeff317cd07ddf0c50e311abdece693105db
Valerian <polskataga@gmail.com>
2014-10-11 16:11:24 +0200

    SuperChat socket.io
    Chat en temps réel utilisant socket.io via Node.js

---
+++ Sources/test/SuperChat/README.md
@@ -0,0 +1,9 @@
+chat en temps réel avec socket.io
+
+Source : http://fr.openclassrooms.com/informatique/cours/des-applications-ultra-rapides-avec-node-js/tp-le-super-chat
+
+Utiliser la commande npm install pour installer les dépendances décrites dans package.json
+
+Puis lancer le projet avec node app.js
+
+Ouvrir 2 onglets http://localhost:8080/ et commencer la discussion
---
+++ Sources/test/SuperChat/app.js
@@ -0,0 +1,29 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    fs = require('fs');
+
+// Chargement de la page index.html
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.set('pseudo', pseudo);
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        socket.get('pseudo', function (error, pseudo) {
+            message = ent.encode(message);
+            socket.broadcast.emit('message', {pseudo: pseudo, message: message});
+        });
+    }); 
+});
+
+server.listen(8080);
