44409aa45878ed1c0005abd37304a06efc26002c
Valerian <polskataga@gmail.com>
2015-03-12 20:25:16 +0100

    Update room.js

--- Prototype/stable version/site/js/room.js
+++ Prototype/stable version/site/js/room.js
@@ -34,7 +34,7 @@ var localMedia = media({
 });
 
 // Connexion à socket.io
-var socket = io.connect('http://'+location.hostname + ':3000');
+var socket = io.connect();
 
 // On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
 var pseudo = prompt('Quel est votre pseudo ?');
